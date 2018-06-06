9c28e8606e44535106a12b16cd4b60e759667599
Dan Hahn <daniel.hahn@michaelkors.com>
2018-06-06 17:11:20 -0400

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,24 @@ module.exports = {
               // Avoids sending pageview hits from custom paths
               exclude: ["/files/**"]
             }
+          },
+          {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+              logo: "./src/favicon.png",
+              injectHTML: true,
+              icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                twitter: false,
+                yandex: false,
+                windows: false
+              }
+            }
           }
         ]
       }
